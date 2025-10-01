33eccd4af39abd17ff2fa5e5097315e7f74f7cd8
Jungyu Park <parkjungyu09@gmail.com>
2025-10-01 16:14:05 +0900

    Chore: 프로젝트 구조 세팅

---
+++ src/shared/config/env.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const ENV = z.object({
+  NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string().min(10),
+}).parse({
+  NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+});
