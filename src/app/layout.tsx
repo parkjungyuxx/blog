d72440adbd350aed675f4ad42863744cade3e4d7
Jungyu Park <parkjungyu09@gmail.com>
2025-10-03 00:39:28 +0900

    Style:  랜딩 페이지 퍼블리싱 완료

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,52 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const jetbrainsMono = localFont({
+  src: [
+    {
+      path: "../../public/fonts/JetBrainsMono-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/JetBrainsMono-ExtraLight.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/JetBrainsMono-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/JetBrainsMono-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/JetBrainsMono-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/JetBrainsMono-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/JetBrainsMono-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/JetBrainsMono-ExtraBold.woff2",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  variable: "--font-jetbrains-mono",
+});
 
 export const metadata: Metadata = {
   title: "micky.log",
@@ -13,7 +60,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className="min-h-dvh antialiased bg-gray-100 text-black">
+      <body
+        className={`${jetbrainsMono.variable} antialiased bg-gray-100 text-gray-900 font-sans`}
+      >
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,37 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <div className="grid grid-cols-2 min-h-screen max-w-7xl mx-auto px-16">
-        <div className="flex flex-col justify-center">
-          <div className="pl-20">
-            <h1 className="text-[72px] font-bold text-black mb-6">micky.log</h1>
-            <p className="text-black text-[32px]">
-              I am a developer who always thinks, communicates, and solves
-              problems.
-            </p>
+    <div className="bg-gray-100 py-8">
+      <div className="grid grid-cols-2">
+        <div className="pl-8 flex flex-col gap-20">
+          <div>
+            <h1 className="text-[144px] font-bold text-black">(+82)</h1>
+            <h1 className="text-[144px] font-bold text-black">micky.log</h1>
           </div>
+          <p className="text-black text-[28px] leading-snug">
+            I am a developer who always{" "}
+            <span className="underline inline-block">thinks</span>,{" "}
+            <span className="underline inline-block">communicates</span>, and{" "}
+            <span className="underline inline-block">solves problems</span>.
+          </p>
         </div>
 
-        <div className="flex flex-col justify-center gap-6">
+        <div className="flex flex-col gap-4">
           <Link
             href="/"
-            className="text-[48px] text-black font-medium transition-colors"
+            className="text-[40px] text-black font-medium transition-colors"
           >
             Resume
           </Link>
           <Link
             href="/posts"
-            className="text-[48px] text-black  font-medium transition-colors"
+            className="text-[40px] text-black font-medium transition-colors"
           >
             Post
           </Link>
           <Link
             href="/about"
-            className="text-[48px] text-black font-medium transition-colors"
+            className="text-[40px] text-black font-medium transition-colors"
           >
             About
           </Link>
